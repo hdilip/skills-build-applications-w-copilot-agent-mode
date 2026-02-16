f2c3c5911b3f5d79c902c302bc825e98e3f02f6a
hdilip <39915082+hdilip@users.noreply.github.com>
2026-02-16 18:00:52 +0000

    fix(api): set request.user automatically in perform_create for Profile and Activity

--- octofit-tracker/backend/activities_app/views.py
+++ octofit-tracker/backend/activities_app/views.py
@@ -8,3 +8,6 @@ class ActivityViewSet(viewsets.ModelViewSet):
 	queryset = Activity.objects.all()
 	serializer_class = ActivitySerializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
--- octofit-tracker/backend/users_app/views.py
+++ octofit-tracker/backend/users_app/views.py
@@ -8,3 +8,6 @@ class ProfileViewSet(viewsets.ModelViewSet):
 	queryset = Profile.objects.all()
 	serializer_class = ProfileSerializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
